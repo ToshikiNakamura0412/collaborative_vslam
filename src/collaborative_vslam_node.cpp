070b3a39976048601424d4310dfa90078c53b5ac
Toshiki Nakamura <ee193014@meiji.ac.jp>
2022-12-12 12:47:40 +0900

    Creat a new file

---
+++ src/collaborative_vslam_node.cpp
@@ -0,0 +1,10 @@
+#include "collaborative_vslam.h"
+
+int main(int argc, char* argv[])
+{
+    ros::init(argc, argv, "collaborative_vslam");
+    CollaborativeVSLAM collaborative_vslam;
+    collaborative_vslam.process();
+
+    return 0;
+}
