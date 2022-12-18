54c04512f30de15313a2710c577ea871c0690f2d
Toshiki Nakamura <ee193014@meiji.ac.jp>
2022-12-18 20:33:56 +0900

    Fix

--- include/collaborative_vslam/point.h
+++ include/collaborative_vslam/point.h
@@ -1,29 +1,33 @@
 #ifndef POINT_H
 #define POINT_H
 
-#include <cmath>
+#include <geometry_msgs/Point.h>
 
 class Point
 {
 public:
     Point(); // デフォルトコンストラクタ
-    Point(const double z, const double x); // コンストラクタ
+    Point(const double x, const double z);   // コンストラクタ
+    Point(const geometry_msgs::Point point); // コンストラクタ
     Point& operator =(const Point& point); // 代入演算子
     Point& operator /=(const double a); // 複合代入演算子/=
     Point& operator *=(const double a); // 複合代入演算子*=
-    const Point operator +(const Point& point) const; // 算術演算子+
-    const Point operator -(const Point& point) const; // 算術演算子-
-    const Point operator *(const Point& point) const; // 算術演算子*
-    const Point operator /(const Point& point) const; // 算術演算子/
+    Point operator +(const Point& point) const; // 算術演算子+
+    Point operator -(const Point& point) const; // 算術演算子-
+    Point operator *(const Point& point) const; // 算術演算子*
+    Point operator /(const Point& point) const; // 算術演算子/
+    friend Point operator *(const Point& point, const double a); // 算術演算子*
+    friend Point operator /(const Point& point, const double a); // 算術演算子/
 
     // accessor
-    void set(const double z, const double x);
-    double z() const { return z_; }
+    void set(const double x, const double z);
+    void set(const geometry_msgs::Point point);
     double x() const { return x_; }
+    double z() const { return z_; }
 
 private:
-    double z_; // [m]
     double x_; // [m]
+    double z_; // [m]
 };
 
 #endif
--- src/point.cpp
+++ src/point.cpp
@@ -1,74 +1,99 @@
-#include "localizer/point.h"
+#include "collaborative_vslam/point.h"
 
 // デフォルトコンストラクタ
 Point::Point()
 {
-    z_ = 0.0;
     x_ = 0.0;
+    z_ = 0.0;
 }
 
 // コンストラクタ
-Point::Point(const double z, const double x)
+Point::Point(const double x, const double z)
 {
-    z_ = z;
     x_ = x;
+    z_ = z;
+}
+Point::Point(const geometry_msgs::Point point)
+{
+    x_ = point.x;
+    z_ = point.z;
 }
 
 // 代入演算子
 Point& Point::operator =(const Point& point)
 {
-    z_ = point.z_;
     x_ = point.x_;
+    z_ = point.z_;
     return *this;
 }
 
 // 複合代入演算子
 Point& Point::operator /=(const double a)
 {
-    z_ /= a;
     x_ /= a;
+    z_ /= a;
     return *this;
 }
 Point& Point::operator *=(const double a)
 {
-    z_ /= a;
     x_ /= a;
+    z_ /= a;
     return *this;
 }
 
 // 算術演算子
-const Point Point::operator +(const Point& point) const
+Point Point::operator +(const Point& point) const
 {
     Point tmp;
+    tmp.x_ = x_ + point.x_;
     tmp.z_ = z_ + point.z_;
-    tmp.x_ = x_ + point.z_;
     return tmp;
 }
-const Point Point::operator -(const Point& point) const
+Point Point::operator -(const Point& point) const
 {
     Point tmp;
+    tmp.x_ = x_ - point.x_;
     tmp.z_ = z_ - point.z_;
-    tmp.x_ = x_ - point.z_;
     return tmp;
 }
-const Point Point::operator *(const Point& point) const
+Point Point::operator *(const Point& point) const
 {
     Point tmp;
+    tmp.x_ = x_ * point.x_;
     tmp.z_ = z_ * point.z_;
-    tmp.x_ = x_ * point.z_;
     return tmp;
 }
-const Point Point::operator /(const Point& point) const
+Point Point::operator /(const Point& point) const
 {
     Point tmp;
+    tmp.x_ = x_ / point.x_;
     tmp.z_ = z_ / point.z_;
-    tmp.x_ = x_ / point.z_;
+    return tmp;
+}
+Point operator *(const Point& point, const double a) // friend
+{
+    Point tmp;
+    tmp.x_ = point.x_ * a;
+    tmp.z_ = point.z_ * a;
+    return tmp;
+}
+
+Point operator /(const Point& point, const double a) // friend
+{
+    Point tmp;
+    tmp.x_ = point.x_ / a;
+    tmp.z_ = point.z_ / a;
     return tmp;
 }
 
 // setter
-void Point::set(const double z, const double x)
+void Point::set(const double x, const double z)
 {
-    z_ = z;
     x_ = x;
+    z_ = z;
+}
+void Point::set(const geometry_msgs::Point point)
+{
+    x_ = point.x;
+    z_ = point.z;
 }
